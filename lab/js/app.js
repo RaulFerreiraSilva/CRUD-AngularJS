3f057d01795450d595ccfb78bc0426a7bfa2a18b
RaulFerreiraSilva <raul.etec.2019@gmail.com>
2022-11-14 13:40:23 -0300

    Add Back-end

--- lab/js/app.js
+++ lab/js/app.js
@@ -1,12 +1,13 @@
 var crud = angular.module("crud", [])
 
-crud.controller("controller", function ($scope) { // scope ligação da view com o controller
+crud.controller("controller", function ($scope, $http) { // scope ligação da view com o controller
 
     $scope.novoContato = {} // array vazio
     $scope.contatoSelecionado = {}
     $scope.novoEndereco = {}
     $scope.enderecoContato = {}
 
+    conexao()
 
     $scope.contatos = [
         {
@@ -20,11 +21,64 @@ crud.controller("controller", function ($scope) { // scope ligação da view com
     $scope.enderecos = [
         {
             logradouro: "Rua Alterosa", numero: 817, bairro: "Bairro Alto", cidadeUF: "Curitiba-PR"
-        }, 
+        },
         {
             logradouro: "Rua Professor Oliverios Vilaça", numero: 456, bairro: "Cidade Industrial", cidadeUF: "Curitiba-PR"
         }
     ]
+
+    function conexao() {
+        $http({
+            url: `https://www.selida.com.br/avaliacaotecnica/api/Pessoas/GetAll`,
+            method: 'GET',
+            headers: {
+                Chave: "D3ACDDF7-7CD7-4CA0-B65B-3EEE92396801"
+            }
+        }).then(function successCallback(response) {
+
+            console.log(response)
+
+        }, function errorCallback(err) {
+            console.error(err);
+        })
+    }
+
+    $scope.contatos = $scope.List = function () {
+        $http({
+            url: `https://www.selida.com.br/avaliacaotecnica/api/Pessoas/GetAll`,
+            method: 'GET',
+            headers: {
+                Chave: "D3ACDDF7-7CD7-4CA0-B65B-3EEE92396801"
+            }
+        }).then(function successCallback(response) {
+
+            console.log(response)
+
+        }, function errorCallback(err) {
+            console.error(err);
+        })
+    }
+
+
+    $scope.save = function () {
+        var payload = novoContato
+
+        $http({
+            url: `https://www.selida.com.br/avaliacaotecnica/api/Pessoas`,
+            method: 'POST',
+            data: payload,
+            headers: {
+                Chave: "D3ACDDF7-7CD7-4CA0-B65B-3EEE92396801"
+            }
+        }).then(function successCallback(response) {
+
+            console.log(response)
+
+        }, function errorCallback(err) {
+            console.error(err);
+        })
+    }
+
 
     $scope.inserirContato = function () { // jogando as info dentro do array
         $scope.contatos.push($scope.novoContato)
@@ -38,16 +92,48 @@ crud.controller("controller", function ($scope) { // scope ligação da view com
 
     $scope.selecionaContato = function (contato) {
         $scope.contatoSelecionado = contato
+
+        $http({
+            url: `https://www.selida.com.br/avaliacaotecnica/api/Pessoas/{pessoaId}`,
+            method: 'PUT',
+            data: contatoSelecionado,
+            headers: {
+                Chave: "D3ACDDF7-7CD7-4CA0-B65B-3EEE92396801"
+            }
+        }).then(function successCallback(response) {
+
+            console.log(response)
+
+        }, function errorCallback(err) {
+            console.error(err);
+        })
+    }
+
+    $scope.delete = function () {
+        $http({
+            url: `https://www.selida.com.br/avaliacaotecnica/api/Pessoas/{pessoaId}`,
+            method: 'DELETE',
+            data: contatoSelecionado,
+            headers: {
+                Chave: "D3ACDDF7-7CD7-4CA0-B65B-3EEE92396801"
+            }
+        }).then(function successCallback(response) {
+
+            console.log(response)
+
+        }, function errorCallback(err) {
+            console.error(err);
+        })
     }
 
     $scope.deletaContato = function () { // metodo aplice alterar ou excluir algo de um array
-        $scope.contatos.splice($scope.contatos.indexOf($scope.contatoSelecionado), 1) 
+        $scope.contatos.splice($scope.contatos.indexOf($scope.contatoSelecionado), 1)
         $scope.enderecos.splice($scope.enderecos.indexOf($scope.contatoSelecionado), 1)
-        
+
         // sintax do splice(pego o array de contato.pego o index do contato selecionado e informa quantos ira excluir apartir dele)
     }
 
-    $scope.ordenarPor = function(campo){
+    $scope.ordenarPor = function (campo) {
         $scope.criterioDeOrdenacao = campo
         $scope.direcao = !$scope.direcao
     }
